3e892bdc219a2734e23a0cf52ff6e64f83cd2578
Francisco Krause Arnim <fkrausear@gmail.com>
2022-06-07 15:58:59 -0300

    Working Phoenix + Riack

---
+++ config/gpad_2.exs
@@ -0,0 +1,8 @@
+import Config
+
+config :riak_core,
+  node: 'gpad_2@127.0.0.1',
+  web_port: 8198,
+  handoff_port: 8199,
+  ring_state_dir: 'ring_data_dir_1',
+  platform_data_dir: 'data_1'
---
+++ lib/riax/mailer.ex
@@ -0,0 +1,3 @@
+defmodule Riax.Mailer do
+  use Swoosh.Mailer, otp_app: :riax
+end
