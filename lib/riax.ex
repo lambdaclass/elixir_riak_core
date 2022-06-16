7ca4a7d305d055932264a937f54d95779efa7d39
Francisco Krause Arnim <fkrausear@gmail.com>
2022-06-16 18:32:42 -0300

    Refactor riax.ex to use VNode implementation

--- lib/riax.ex
+++ lib/riax.ex
@@ -10,5 +10,27 @@ defmodule Riax do
   def keys() do
     sync_command(:key, :keys)
   end
+
+  def ping() do
+    ping(:os.timestamp())
+  end
+
+  def ping(key) do
+    sync_command(key, {:ping, 1})
+  end
+
+  defp sync_command(key, command) do
+    # Get the key's hash
+    doc_idx = hash_key(key)
+    # Get the prefered node for the given key
+    preflist = :riak_core_apl.get_apl(doc_idx, 1, :riax_service)
+    [index_node] = preflist
+    :riak_core_vnode_master.sync_spawn_command(index_node, command, Riax.VNode_master)
+  end
+
+  defp hash_key(key), do: hash_key(key, <<"riak">>)
+
+  defp hash_key(key, bucket) when is_binary(bucket) do
+    :riak_core_util.chash_key({bucket, :erlang.term_to_binary(key)})
   end
 end
