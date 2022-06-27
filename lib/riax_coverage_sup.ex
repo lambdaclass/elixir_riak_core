0afac0f853ef5c9ee1aa759c4f463de2ec9a9298
Francisco Krause Arnim <fkrausear@gmail.com>
2022-06-27 16:04:50 -0300

    Add coverage supervisor

---
+++ lib/riax_coverage_sup.ex
@@ -0,0 +1,27 @@
+defmodule Riax.CoverageSup do
+  @behaviour Supervisor
+  def start_link() do
+    Supervisor.start_link( __MODULE__, [], name: :coverage_sup)
+  end
+
+  def init([]) do
+    # coverage_fsm =
+    #   {:undefined, {Riax.CoverageFsm, :start_link, []}, :temporary, 5000, :worker,
+    #    [Riax.CoverageFsm]}
+
+    children = [
+      %{
+        id: :undefined,
+        start: {Riax.CoverageSup, :start_link, [Riax.CoverageSup]},
+        restart: :temporary,
+        type: :worker
+      }
+    ]
+
+    Supervisor.init(children, strategy: :simple_one_for_one, max_restarts: 10, max_seconds: 10)
+  end
+
+  def start_fsm(args) do
+    Supervisor.start_child(__MODULE__, args)
+  end
+end
