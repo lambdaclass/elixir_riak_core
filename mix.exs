4ad7bf517363081aff9fe2a6fd85e210cc865c88
Francisco Krause Arnim <fkrausear@gmail.com>
2022-07-01 19:09:43 -0300

    Add CSV dep

--- mix.exs
+++ mix.exs
@@ -18,11 +18,11 @@ defmodule Riax.MixProject do
   #
   # Type `mix help compile.app` for more information.
   def application do
-      [
-        mod: {Riax.Application, []},
-        applications: [:phoenix, :cowboy, :riak_core],
-        extra_applications: [:logger, :runtime_tools]
-      ]
+    [
+      mod: {Riax.Application, []},
+      applications: [:phoenix, :cowboy, :riak_core],
+      extra_applications: [:logger, :runtime_tools]
+    ]
   end
 
   # Specifies which paths to compile per environment.
@@ -50,8 +50,8 @@ defmodule Riax.MixProject do
       {:cuttlefish,
        git: "https://github.com/fkrause98/cuttlefish", manager: :rebar3, override: true},
       {:hut, "~> 1.3", manager: :rebar3, override: true},
-      {:riak_core, manager: :rebar3, git: "https://github.com/basho/riak_core", ref: "develop"}
-      # {:rc_example, manager: :rebar3, git: "https://github.com/lambdaclass/riak_core_tutorial", ref: "update-repo-files"}
+      {:riak_core, manager: :rebar3, git: "https://github.com/basho/riak_core", ref: "develop"},
+      {:csv, "~> 2.4"}
     ]
   end
 
