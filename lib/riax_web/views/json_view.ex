7ee1892632c0d655f80ff081ca59f01eb42e46ba
Francisco Krause Arnim <fkrausear@gmail.com>
2022-07-06 20:08:55 -0300

    Remove Inspect

--- lib/riax_web/views/json_view.ex
+++ lib/riax_web/views/json_view.ex
@@ -2,7 +2,6 @@ defmodule RiaxWeb.APIView do
   use RiaxWeb, :controller
 
   def render("index.json", %{conn: %{assigns: %{tweet: tweet}}}) do
-    IO.inspect(tweet, label: :PARAMS)
     tweet
   end
 end
