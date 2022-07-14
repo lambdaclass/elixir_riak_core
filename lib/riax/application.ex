62b8ecc8f843a857315a5b5962ac2da9124d989c
Francisco Krause Arnim <fkrausear@gmail.com>
2022-07-14 12:24:34 -0300

    Modify config as to make tests work

--- lib/riax/application.ex
+++ lib/riax/application.ex
@@ -7,15 +7,6 @@ defmodule Riax.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Start the Telemetry supervisor
-      RiaxWeb.Telemetry,
-      # Start the PubSub system
-      {Phoenix.PubSub, name: Riax.PubSub},
-      # Start the Endpoint (http/https)
-      {RiaxWeb.Endpoint, name: Riax.Endpoint},
-      # Start Riak's supervisor,
-      # vnode: the vnode implementation,
-      # coverage: the supervisor for the coverage.
       Riax.Supervisor
     ]
 
@@ -23,13 +14,5 @@ defmodule Riax.Application do
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Riax.Supervisor]
     Supervisor.start_link(children, opts)
-  end
-
-  # Tell Phoenix to update the endpoint configuration
-  # whenever the application is updated.
-  @impl true
-  def config_change(changed, _new, removed) do
-    RiaxWeb.Endpoint.config_change(changed, removed)
-    :ok
   end
 end
