9039073a1fe7e9cd91feb3831efdd2ea783e426c
Francisco Krause Arnim <fkrausear@gmail.com>
2022-07-14 13:20:50 -0300

    Add ex_docs

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule Riax.MixProject do
       {:local_cluster, "~> 1.2", only: [:test]},
       {:hackney, "~> 1.9"},
       {:parse_trans, "~> 3.4.1", override: true},
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.27", only: [:dev, :test], runtime: false},
     ]
   end
 
